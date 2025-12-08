25b89eabd9380bd20bf969c1b82b6207fb0779b6
Aysha Aziz <aishaazizz30@gmail.com>
2025-12-08 21:06:31 +0400

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import config from "./config";
+import cors from 'cors';
 import initDB, { pool } from "./config/db";
 import logger from "./middleware/logger";
 import { usersRoutes } from "./modules/users/users.routes";
@@ -11,6 +12,11 @@ const app = express()
 const port = config.port;
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5000', // your frontend URL
+  credentials: true,               // allow cookies
+}));
 
 //initialize db
 initDB();
